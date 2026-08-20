3299d4d2daa9807f9ed93be110fc60a188b2925c
Will Louis <sonicspider12@gmail.com>
2026-08-20 15:52:59 +1000

    HealthSystemUpdate

--- Celestial Slayer (Unity File)/Assets/Scripts/Player/PlayerHealth.cs
+++ Celestial Slayer (Unity File)/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,19 +1,73 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     private enum HealthSystem { regenHealth, healthPoints };
-    [Header("Health Stats")]
-    [SerializeField] private HealthSystem healthSystem;
-    [SerializeField] private float maxPlayerHealth;
-    public static float health;
-    [SerializeField] float timeTillHealthregenSec;
-    [SerializeField] float healthRegenPerSecond;
+    private float timeSinceLastDamaged;
+    private bool hit;
+    private float health;
 
-    void Die()
+    [SerializeField] private HealthSystem healthSystem;
+    [SerializeField] private bool resetHealth;
+
+    [Header("Health Regen")]
+    [SerializeField] private float maxPlayerHealth = 100;
+    [SerializeField] private float timeTillHealthregenSec;
+    [SerializeField] private float healthRegenPerSecond;
+
+    [Header("Health Points")]
+    [SerializeField] private int totalHitPoints;
+
+
+    private void Update()
     {
+        Debug.Log("Health: " + health);
+        if(resetHealth)
+            health = maxPlayerHealth;
+
+        if(health < maxPlayerHealth)
+        {
+            if(healthSystem == HealthSystem.regenHealth) 
+                RegenHealth();
+            else if(healthSystem == HealthSystem.healthPoints)
+                HealthPoints();
+        }
 
     }
+
+    private void RegenHealth()
+    {
+        if(hit)
+        {
+            timeSinceLastDamaged = Time.time;
+            hit = false;
+        }
+
+        float damageTime = Time.time - timeSinceLastDamaged;
+
+        if(damageTime >= timeTillHealthregenSec)
+        {
+            health += healthRegenPerSecond * Time.deltaTime;
+        }
+
+        if (health > maxPlayerHealth)
+            health = maxPlayerHealth;
+    }
+
+    private void HealthPoints()
+    {
+        
+
+    }
+
+    public void Hit(float damage)
+    {
+        hit = true;
+        health -= damage;
+    }
+
+
 
 
 }
