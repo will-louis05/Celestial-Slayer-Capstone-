70dfb604fde8c3d19ad40e0066f8e01745bf4fc3
Jude Wheelhouse <judewheels.04@gmail.com>
2026-09-03 11:56:10 +1000

    Update PlayerController.cs

--- Celestial Slayer (Unity File)/Assets/Scripts/Player/PlayerController.cs
+++ Celestial Slayer (Unity File)/Assets/Scripts/Player/PlayerController.cs
@@ -156,8 +156,8 @@ public class PlayerController : MonoBehaviour
 
         if(heldSpear != null)
         {
-            heldSpear.transform.parent = spearFollow;
-            //holdOffset.LookAt(spearFollow);
+            //heldSpear.transform.parent = spearFollow; (Jude)
+            holdOffset.LookAt(spearFollow);
         }
     }
 
@@ -276,8 +276,8 @@ public class PlayerController : MonoBehaviour
                     spearToSpawn = explosiveSpear;
                     break;
             }
-            heldSpear = Instantiate(spearToSpawn, spearFollow);
-            //heldSpear = Instantiate(spearToSpawn, holdOffset);
+            //heldSpear = Instantiate(spearToSpawn, spearFollow); (Jude)
+            heldSpear = Instantiate(spearToSpawn, holdOffset);
             Animator spearAni = heldSpear.GetComponent<Animator>();
             var aniLength = spearAni.GetCurrentAnimatorStateInfo(0).length;
             aniLength = aniLength/ equipTime;
