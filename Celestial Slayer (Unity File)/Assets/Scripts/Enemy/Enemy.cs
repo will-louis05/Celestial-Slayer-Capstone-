083aa534f9cd1717ef2a497f64e3547e42305d97
Will Louis <sonicspider12@gmail.com>
2026-09-24 12:05:10 +1000

    AttackScrptMissingFix

--- Celestial Slayer (Unity File)/Assets/Scripts/Enemy/Enemy.cs
+++ Celestial Slayer (Unity File)/Assets/Scripts/Enemy/Enemy.cs
@@ -13,6 +13,7 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] private float regainSpeed;
     [SerializeField] protected float spearSpeedDecreaseRatio;
     public float damage;
+    public EnemyAttack attackScrpt;
 
     //[SerializeField] private bool enemyDisable;
     //private bool inEnabled;
